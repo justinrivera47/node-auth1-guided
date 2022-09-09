8675db8774b986942af78a62cc6651877998b293
justinrivera47 <justinrivera47@gmail.com>
2022-09-09 11:58:38 -0400

    logout works

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -20,13 +20,30 @@ router.post('/login', async (req,res,next) => {
   try {
     const { username,  password } = req.body
     const [user] = await User.findBy({ username })
-    console.log(user)
+    if (user && bcrypt.compareSync(password, user.password)){
+      req.session.user = user
+      res.json({ message: `Welcome back, ${user.username}`})
+    } else {
+      next({ status: 401, message: 'Invalid username or password'})
+    }
   } catch (error) {
     next(error)
   }
 })
 router.get('/logout', (req,res,next) => {
-    res.json({ message: 'logout working'})
+  if(req.session.user) {
+    const { username } = req.session.user
+    req.session.destroy(err => {
+      if(err) {
+        res.json({ message: `you could never leave, ${username}`})
+      } else {
+        res.set('Set-Cookie', 'monkey=; SameSite=Strict; Path=/; Expires=Thu, 01 Jan 1970 00:00:00')
+        res.json({ message: `Goodbye, ${username}` })
+      }
+    })
+  } else {
+    res.json({ message: 'sorry, have we met?'})
+  }
 })
 
 module.exports = router
